a58d2a259cb1317c9b07f70da3af8067624bc51d
khurshid <rakhamtullaevkhurshid17@gmail.com>
2021-02-20 20:43:19 +0500

    comands

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const comands = [];
 for (let i = 0; i < Infinity; i++) {
     let a = prompt('Введите команду (add, del, stop)', 'add, Иван');
-    comands.push(a)
     if (a=='stop') {
         break;
     }
-    else if (a == "del") {
-        let del = prompt('Какое имя удалит');
+    else if (a.split(", ")[0] == "del") {
         for (let j = 0; j < comands.length; j++) {
-            if(comands[j]==del){
-                comands[j].splice()
+            if(comands[j]==a.split(", ")[1]){
+                comands.splice(j,1);
             }
         }
+    }
+    else if (a.split(", ")[0] == "add") {
+        comands.push(a.split(", ")[1]);
     }
 }
 console.log(comands);
